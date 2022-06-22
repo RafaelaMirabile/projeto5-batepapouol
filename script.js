d41407dd8cb701ba6a35023d195eaacb1fdde950
RafaelaMirabile <rafaelamirabile@gmail.com>
2022-06-22 16:06:01 -0300

    get messages from API complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let userName;
+let messages =[];
 let chat;
 
 function enterChat(){
@@ -6,10 +7,60 @@ function enterChat(){
     let foot = document.querySelector("footer");
     let head = document.querySelector("header");
     chat = document.querySelector(".containerChat");
+
     userName = document.querySelector(".user").value;
     
     login.classList.add("none");
     foot.classList.remove("none");
     chat.classList.remove("none");
     head.classList.remove("none");
+    getMessages();
+}
+
+// funcao para pegar as mensagens da API//
+function getMessages(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(getData);
+}
+
+
+function getData(response){
+messages = response.data;
+console.log(messages);
+messagesOnChat();
+}
+
+function messagesOnChat(){
+    for(i=0; i < messages.length ; i++){
+        if(messages[i].type === "status"){
+                chat.innerHTML+=`<div class = "message status"> 
+                                    <span class = "time">(${messages[i].time})</span>
+                                    <strong> ${messages[i].from}</strong>
+                                    <span> ${messages[i].text}</span>
+                                </div>`;
+        }
+        else{
+            if(messages[i].type === "message"){
+                chat.innerHTML+=`<div class = "message message"> 
+                                    <span class = "time">(${messages[i].time})</span>
+                                    <strong> ${messages[i].from}</strong>
+                                    <span> ${messages[i].text}</span>
+                                </div>`;
+            }
+            else if(messages[i].type === "private_message"){
+                    chat.innerHTML+=`<div class = "message message"> 
+                                        <span class = "time">(${messages[i].time})</span>
+                                        <strong> ${messages[i].from}</strong>
+                                        <span> ${messages[i].text}</span>
+                                    </div>`;
+            }
+        }
+
+    }
+ 
+
+
+            
+
+    
 }
