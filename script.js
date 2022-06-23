b08c5a281633cd60711417cef57d52ae83bc538e
RafaelaMirabile <rafaelamirabile@gmail.com>
2022-06-22 22:47:55 -0300

    Send objet to participants API complete

--- script.js
+++ script.js
@@ -8,28 +8,30 @@ function enterChat(){
     let head = document.querySelector("header");
     chat = document.querySelector(".containerChat");
 
-    userName = document.querySelector(".user").value;
     
     login.classList.add("none");
     foot.classList.remove("none");
     chat.classList.remove("none");
     head.classList.remove("none");
-    getMessages();
+    sendMessage();
+    setInterval(getMessages ,3000 );
+    
 }
 
 // funcao para pegar as mensagens da API//
 function getMessages(){
+    console.log("funcao getMessages indo a cada 3s");
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(getData);
 }
 
-
+//funcao para pegar response//
 function getData(response){
 messages = response.data;
-console.log(messages);
 messagesOnChat();
 }
 
+//funcao para colocar as mensagens no chat //
 function messagesOnChat(){
     for(i=0; i < messages.length ; i++){
         if(messages[i].type === "status"){
@@ -41,26 +43,30 @@ function messagesOnChat(){
         }
         else{
             if(messages[i].type === "message"){
-                chat.innerHTML+=`<div class = "message message"> 
+                chat.innerHTML+=`<div class = "message all"> 
                                     <span class = "time">(${messages[i].time})</span>
-                                    <strong> ${messages[i].from}</strong>
+                                    <strong> ${messages[i].from} </strong>
                                     <span> ${messages[i].text}</span>
                                 </div>`;
             }
             else if(messages[i].type === "private_message"){
-                    chat.innerHTML+=`<div class = "message message"> 
+                    chat.innerHTML+=`<div class = "message private"> 
                                         <span class = "time">(${messages[i].time})</span>
-                                        <strong> ${messages[i].from}</strong>
+                                        <strong> ${messages[i].from}"reservadamente para"${messages[i].to}</strong>
                                         <span> ${messages[i].text}</span>
                                     </div>`;
             }
         }
 
     }
- 
-
-
-            
-
-    
+}
+// mandando o nome para entrar e sair //
+function sendMessage(){
+    let userName = document.querySelector(".user").value;
+    console.log(userName);
+    let object = {
+        name:userName
+      }
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , object );
+    promise.then(getMessages);
 }
