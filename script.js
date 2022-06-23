a5a86815332d343ed4be3dd583a2a007eb9a29c1
RafaelaMirabile <rafaelamirabile@gmail.com>
2022-06-23 13:13:57 -0300

    Ajustments in js flow and setInterval for status implmented

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let userName;
 let messages =[];
 let chat;
+let object;
 
 function enterChat(){
     let login = document.querySelector(".containerLogin");
@@ -13,11 +14,22 @@ function enterChat(){
     foot.classList.remove("none");
     chat.classList.remove("none");
     head.classList.remove("none");
-    sendMessage();
-    //setInterval(getMessages ,3000 );
+    sendUserName();
+    setInterval (sendUserName, 5000);
+    setInterval(getMessages ,3000 );
     
 }
 
+function sendUserName(){
+    console.log("enviando userName para parc API cada 5s");
+    let userName = document.querySelector(".user").value;
+    console.log(userName);
+    let object = {
+        name:userName
+      }
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , object );
+    promise.then(getMessages);
+}
 // funcao para pegar as mensagens da API//
 function getMessages(){
     console.log("funcao getMessages indo a cada 3s");
@@ -27,9 +39,9 @@ function getMessages(){
 
 //funcao para pegar response//
 function getData(response){
-messages = response.data;
-messagesOnChat();
-}
+    messages = response.data;
+    messagesOnChat();
+    }
 
 //funcao para colocar as mensagens no chat //
 function messagesOnChat(){
@@ -62,18 +74,28 @@ function messagesOnChat(){
     scroll();
 }
 // mandando o nome para entrar e sair //
-function sendMessage(){
-    let userName = document.querySelector(".user").value;
-    console.log(userName);
-    let object = {
-        name:userName
-      }
-    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , object );
-    promise.then(getMessages);
-}
-// FAZER FUNCIONAR
+
+
 function scroll(){
     const ultima = document.querySelector(".containerChat").lastElementChild;
     console.log(ultima);
     ultima.scrollIntoView();
+}
+
+function sendUserMessage(){
+    let u = document.querySelector(".user").value;
+    let userMessage = document.querySelector(".messageInput").value;
+    let messageObject = {
+        from: u,
+        to: "Todos",
+        text: userMessage,
+        type: "message" 
+    }
+    console.log(userName);
+    const promise = axios.post ("https://mock-api.driven.com.br/api/v6/uol/messages", messageObject)
+    promise.then(getMessages);
+    promise.catch(resolveError);
+}
+function resolveError(){
+    alert("errrrou")
 }
